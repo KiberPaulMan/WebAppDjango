d13ceb87fcb425eb06ea3f1863bbc93df41e18e6
Paul <twinencrypt@gmail.com>
2020-12-24 16:07:50 +0700

    Added condition at the news

--- itproger/news/views.py
+++ itproger/news/views.py
@@ -3,5 +3,5 @@ from .models import Articles
 
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.all().order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
