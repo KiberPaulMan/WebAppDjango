ade2615f7a9827f90631cc680b1195a849b278d4
Paul <putincevpavel777@gmail.com>
2020-12-24 22:07:33 +0700

    Begin create form to add news

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-=0s6o7b6_8g^^(&4!1fb0wh3ftb*@mc#lc^f-f#)q10p-)-dg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.101.4.17']
+ALLOWED_HOSTS = ['127.0.0.1', '10.101.4.17', '192.168.10.4']
 
 
 # Application definition
--- itproger/news/views.py
+++ itproger/news/views.py
@@ -5,3 +5,7 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.all().order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    return render(request, 'news/create.html')
